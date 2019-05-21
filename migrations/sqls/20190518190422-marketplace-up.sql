23a1123c21282ef4b567fc1145cfc38691cce4a7
avinash.vazratkar <avinash.vazratkar@harbingergroup.com>
2019-05-21 21:13:01 +0530

    CHange in database schema.

--- migrations/sqls/20190518190422-marketplace-up.sql
+++ migrations/sqls/20190518190422-marketplace-up.sql
@@ -6,23 +6,54 @@ CREATE TABLE IF NOT EXISTS user_type(
 );
 
 CREATE TABLE IF NOT EXISTS user(
-    first_name VARCHAR(40) NOT NULL,
-    last_name VARCHAR(40) NOT NULL,
-    id VARCHAR(36) PRIMARY KEY,
-    username VARCHAR(20) NOT NULL UNIQUE,
-    password VARCHAR(36) NOT NULL,
+    id VARCHAR(80) PRIMARY KEY,
     email VARCHAR(40) NOT NULL,
-    gender VARCHAR(6) NOT NULL,
-    mobile_number VARCHAR(15) NOT NULL,
     account VARCHAR(80) NOT NULL,
     userType INT NOT NULL,
     uat DATETIME,
     cat DATETIME NOT NULL,
     FOREIGN KEY (userType) REFERENCES user_type(id),
-    CONSTRAINT gender CHECK (gender="male" OR gender='femail' OR gender='other')
+);
+
+CREATE TABLE IF NOT EXISTS farmers(
+    userId VARCHAR(80) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id),
+    UID VARCHAR(40) NOT NULL UINQUE,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    district VARCHAR(20) NOT NULL,
+    village VARCHAR(20) NOT NULL,
+    birth_date date NOT NULL
 );
 
 
+
+
+
+CREATE TABLE IF NOT EXISTS government(
+    userId VARCHAR(80) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id),
+);
+
+CREATE TABLE IF NOT EXISTS dairy_company(
+    userId VARCHAR(80) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id),
+);
+
+CREATE TABLE IF NOT EXISTS veterinarians(
+    userId VARCHAR(80) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id),
+    UID VARCHAR(40) NOT NULL UINQUE,
+    doctor_id VARCHAR(50) NOT NULL,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    district VARCHAR(20) NOT NULL,
+    village VARCHAR(20) NOT NULL,
+    birth_date date NOT NULL,
+    contact VARCHAR(20) NOT NULL
+);
 
 
 INSERT INTO user_type values(1, "Farmers");
