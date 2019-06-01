2ca4d53887a11a53f67f04d10643796d7a64bb65
rushikeshacharya <rushikeshacharya09@gmail.com>
2019-06-01 11:40:37 +0530

    farmer-contract test cases

--- test/farmer.test.js
+++ test/farmer.test.js
@@ -30,33 +30,93 @@ contract('Farmer Registration', async (accounts) => {
 	let totalSupply = 10000;
 
 	before(async () => {
-		cattleInstance = Cattle.new(cattleName, cattleSymbol);
-		milkInstance = Milk.new(milkName, milkSymbol);
-		govInstance = Gov.new();
-		marketPlaceInstance = MarketPlaceToken.new(tokenName, tokenSymbol, decimals, accounts[0], totalSupply)
-		auctionInstance = Auction.new(cattleInstance.address, marketPlaceInstance.address, milkInstance.address);
-		farmerInstance = Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, milkInstance.address);
+		cattleInstance = await Cattle.new(cattleName, cattleSymbol);
+		milkInstance = await Milk.new(milkName, milkSymbol);
+		govInstance = await Gov.new();
+		marketPlaceInstance = await MarketPlaceToken.new(tokenName, tokenSymbol, decimals, accounts[0], totalSupply)
+		auctionInstance = await Auction.new(cattleInstance.address, marketPlaceInstance.address, milkInstance.address);
+		farmerInstance = await Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, milkInstance.address);
+
+		console.log('Cattle : ', cattleInstance.address);
+		console.log('Milk : ', milkInstance.address);
+		console.log('Gov : ', govInstance.address);
+		console.log('MarketPlace  : ', marketPlaceInstance.address);
+		console.log('Auction : ', auctionInstance.address);
+		console.log('Farmer : ', farmerInstance.address);
 
 	})
 
 	describe('Test Farmer Registration', () => {
 		describe('Test Constructor', () => {
 
-			describe('when the cattle contract address is ZERO address', () => {
+			/**
+			 * Positive scenario test cases
+			 */
+			describe('Cattle Contract Address should not be ZERO', () => {
 
-
-				it('reverts', async () => {
-				console.log('Cattle : ', cattleInstance.address);
-				console.log('Milk : ', milkInstance.address);
-				console.log('Gov : ', govInstance.address);
-				console.log('MarketPlace  : ', marketPlaceInstance.address);
-				console.log('Auction : ', auctionInstance.address);
-				console.log('Farmer : ', farmerInstance.address);
-
-				// await expectRevert(Farmer.new(ZERO_ADDRESS, auctionInstance.address, govInstance.address, milkInstance.address), 'Cattle Contract address is ZERO_ADDRESS');
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, milkInstance.address), 'Cattle Contract address is not ZERO_ADDRESS');
 
 				});
 
+			})
+
+			describe('Cattle Should be the Contract Address', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(accounts[0], auctionInstance.address, govInstance.address, milkInstance.address), 'Cattle Contract address should be contract address');
+
+				});
+
+			})
+
+			describe('Auction Contract Address should not be ZERO', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, milkInstance.address), 'Auction Contract address is not ZERO_ADDRESS');
+
+				});
+			})
+
+			describe('Auction Should be the Contract Address', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, accounts[0], govInstance.address, milkInstance.address), 'Auction Contract address should be contract address');
+
+				});
+			})
+
+
+			describe('Government Contract Address should not be ZERO', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, milkInstance.address), 'Government Contract address is not ZERO_ADDRESS');
+
+				});
+			})
+
+			describe('Government Should be the Contract Address', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, auctionInstance.address, accounts[0], milkInstance.address), 'Government Contract address should be contract address');
+
+				});
+			})
+
+			describe('Milk Contract Address should not be ZERO', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, milkInstance.address), 'Milk Contract address is not ZERO_ADDRESS');
+
+				});
+			})
+
+			describe('Milk Should be the Contract Address', () => {
+
+				it('should not revert ', async () => {
+					await expect(Farmer.new(cattleInstance.address, auctionInstance.address, govInstance.address, accounts[0]), 'Milk Contract address should be contract address');
+
+				});
 			})
 
 		})
